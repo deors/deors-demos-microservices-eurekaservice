c9221bfc3a058a19323de9d71825fe8c6c145c1b
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2017-10-24 18:47:12 +0200

    added actuator dependency
    also replaced tabs to spaces

--- src/main/java/deors/demos/microservices/eurekaservice/EurekaserviceApplication.java
+++ src/main/java/deors/demos/microservices/eurekaservice/EurekaserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 public class EurekaserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserviceApplication.class, args);
+    }
 }
